546cf9421ce3c88851d5b2baed83e6cad7cde6fb
rezkyyustisio <rezky.yustisio@gmail.com>
2026-03-02 22:24:54 +0700

    fix-bug-store-berita

--- app/Models/Berita/Berita.php
+++ app/Models/Berita/Berita.php
@@ -5,26 +5,11 @@ namespace App\Models\Berita;
 use App\Models\User;
 use Haruncpi\LaravelUserActivity\Traits\Loggable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 
 class Berita extends Model
 {
     use Loggable;
     protected $fillable = ['category_id','name','slug','description','tags','top','highlight','image','created_by','created_at'];
-    public $incrementing = false;
-    protected $keyType = 'string';
-
-    public static function boot()
-    {
-        parent::boot();
-        static::creating(function ($model) {
-            if (empty($model->id)) {
-                $model->id = (string) Str::uuid();
-                $model->created_by = Auth::id();
-            }
-        });
-    }
 
     public function category(){
         return $this->belongsTo(BeritaCategory::class,'category_id');
